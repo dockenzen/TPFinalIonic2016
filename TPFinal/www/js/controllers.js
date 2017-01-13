24242109847599e781575b49ce37b2d7a079bc6b
dockenzen <lea_eldoc@hotmail.com>
2017-01-13 07:35:52 -0300

    Github login

--- TPFinal/www/js/controllers.js
+++ TPFinal/www/js/controllers.js
@@ -144,6 +144,42 @@ try{
     console.info("Ha ocurrido un error en LoginCtrl. " + error);
   }
 
+$scope.Github = function(){
+    var provider = new firebase.auth.GithubAuthProvider();
+    provider.addScope('repo');
+
+    console.log(provider);
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var usuario = firebase.auth().currentUser;
+
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+  // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+  // ...
+    }).catch(function(error) {
+  // Handle Errors here.  
+
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(error);
+  if(errorCode == 'auth/account-exists-with-different-credential')
+  {
+
+
+  }
+  // ...
+});
+
+
+};
+
 
 $scope.Loguear = function (){
     $scope.mensajeLogin.ver = false;
